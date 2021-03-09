e58e29b06bf70182a4933bf4b68cbedee1bc4c73
jasonvranek <jvranek@ucsc.edu>
2021-03-08 16:00:18 -0800

    install script for mac

---
+++ mac_install.sh
@@ -0,0 +1,15 @@
+python3 -m virtualenv --python=/usr/local/bin/python3 chisel_nb_env
+source chisel_nb_env/bin/activate
+pip install -U chisel-nbdev
+pip install jupyterlab
+curl -L -o coursier https://git.io/coursier-cli && chmod +x coursier
+SCALA_VERSION=2.12.10 ALMOND_VERSION=0.9.1
+./coursier bootstrap -r jitpack \
+    -i user -I user:sh.almond:scala-kernel-api_$SCALA_VERSION:$ALMOND_VERSION \
+    sh.almond:scala-kernel_$SCALA_VERSION:$ALMOND_VERSION \
+    --sources --default=true \
+    -o almond
+./almond --install
+rm almond
+rm coursier
+
